923c197e1bdd0e6e0c2b66d647e5bd6d18a91d02
50275 <68676339+50275@users.noreply.github.com>
2025-09-24 17:01:17 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,2 @@
 Blocks.landingPad.buildType = () => extend(LandingPad.LandingPadBuild, Blocks.landingPad, {accessible: function(){ return true; }})
+Vars.state.rules.revealedBlocks.addAll(Blocks.advancedLaunchPad, Blocks.landingPad);
